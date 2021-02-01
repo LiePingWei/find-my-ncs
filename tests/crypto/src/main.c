5650cd90691a5cd26f763f7ac57a30b90b8991b0
Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>
2021-02-01 15:12:12 +0100

    NCSDK-7745: tests: add unit tests for fmn crypto

---
+++ tests/crypto/src/main.c
@@ -0,0 +1,23 @@
+#include <tc_util.h>
+#include <zephyr.h>
+#include <ztest.h>
+
+void test_collab(void);
+void test_decrypt(void);
+void test_ecdsa(void);
+void test_ecies(void);
+void test_keyroll(void);
+void test_ssecret(void);
+
+/*test case main entry*/
+void test_main(void)
+{
+	ztest_test_suite(test_fmn_crypto,
+		ztest_unit_test(test_collab),
+		ztest_unit_test(test_decrypt),
+		ztest_unit_test(test_ecdsa),
+		ztest_unit_test(test_ecies),
+		ztest_unit_test(test_keyroll),
+		ztest_unit_test(test_ssecret));
+	ztest_run_test_suite(test_fmn_crypto);
+}
